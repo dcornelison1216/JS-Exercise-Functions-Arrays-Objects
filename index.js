aae989b764e5e5d8f1658b0276a5510c84ede79e
derek-cornelison <dcornelison1216@gmail.com>
2020-02-05 06:04:43 -0500

    Tests 22, 23 and 24 pass (stretch goals)

--- index.js
+++ index.js
@@ -326,8 +326,15 @@ const argTimesTwo = (num) => num*2;
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(num) {
+  const car = {
+    odometer: num,
+    drive: function(distance) {
+      this.odometer = this.odometer + distance;
+      return this.odometer;
+    }
+  };
+  return car;
 }
 
 /// ////// END OF CHALLENGE /////////
